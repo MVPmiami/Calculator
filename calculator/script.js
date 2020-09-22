46d5aecfb6b4320144c1fa24d9a2bd0894c3374b
Aliaksei Siankevich <andfly@tut.by>
2020-09-22 16:22:08 +0300

    feat: add global variables

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,28 @@
-alert(`lol`);
+let numbers = document.querySelectorAll(".number");
+let operations = document.querySelectorAll(".operator");
+let decimalBtn = document.getElementById("decimal");
+let c = document.getElementById("c");
+let ce = document.getElementById("ce");
+let result = document.getElementById("result");
+
+console.log(decimalBtn);
+console.log(c);
+console.log(ce);
+console.log(result);
+
+
+function numberPress() {
+
+};
+
+function operation() {
+
+};
+
+function decimal() {
+
+};
+
+function clear() {
+
+};
